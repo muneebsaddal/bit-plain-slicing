668652f9a4ab53e58c75260b156b2c8e1e8d723f
Muneeb Saddal <muneebsaddal@outlook.com>
2019-03-04 23:39:33 +0500

    made a generic method for all image color types

--- imageNegative.py
+++ imageNegative.py
@@ -1,40 +1,13 @@
 from PIL import Image
+import numpy as np
 
-#Converting to Negative -- for RBG image
-img_file = Image.open("lena_rgb.png")
-img = img_file.load()
-width, height = img_file.size
+#opening image
+img = Image.open("lena_rgb.png")
 
-for x in range(width):
-    for y in range(height):
-        [r, g, b] = img[x, y]
-        sr = 255 - r
-        sg = 255 - g
-        sb = 255 - b
-        img[x, y] = (sr, sg, sb)
-img_file.save("lena_negative.png")
+#getting negative of image
+color_val = np.array(img)
+color_val = 255 - color_val
+img = Image.fromarray(color_val)
 
-#Converting to Negative -- for grayscale image
-img_file = Image.open("lena_rgb.png").convert('L')
-img = img_file.load()
-width, height = img_file.size
-
-for x in range(width):
-    for y in range(height):
-        gray_val = img[x, y]
-        new_val = 255 - gray_val
-        img[x, y] = new_val
-img_file.save("lena_grayscale_negative.png")
-
-
-#Converting to Negative -- for binarized image
-img_file = Image.open("lena_rgb.png").convert('L').point((lambda x : 255 if x > 150 else 0), mode = '1')
-img = img_file.load()
-width, height = img_file.size
-
-for x in range(width):
-    for y in range(height):
-        bi_val = img[x, y]
-        new_bi_val = 255 - bi_val
-        img[x, y] = new_bi_val
-img_file.save("lena_binarized_negative.png")
+#saving new image
+img.save("lena_negative.png")
