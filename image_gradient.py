9ee0c19ca57bf0f8342962595ae1039a0a647a1a
Muneeb Saddal <muneebsaddal@outlook.com>
2019-03-04 23:48:32 +0500

    completed

--- image_gradient.py
+++ image_gradient.py
@@ -2,10 +2,10 @@ from PIL import Image
 
 img_file = Image.open('lena_rgb.png').convert('L')
 img = img_file.load()
-width, height = img_file.size
+row, col = img_file.size
 
-for x in range(width-1):
-    for y in range(height):
+for x in range(row-1):
+    for y in range(col):
         gradient = img[x, y] - img[x+1, y]
         img[x, y] = gradient
 img_file.save("lena_gradient.png")
